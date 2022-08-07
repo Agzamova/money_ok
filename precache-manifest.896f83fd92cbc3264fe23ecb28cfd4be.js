7028dcf927b5e212a67ca0695e7f573b3a2f7207
Agzamova <agzamova@inbox.ru>
2022-08-07 14:01:42 +0500

    Updates

---
+++ precache-manifest.896f83fd92cbc3264fe23ecb28cfd4be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ea8c142ee03aa56b1f1921649f9cd1d",
+    "url": "/money_ok/index.html"
+  },
+  {
+    "revision": "eb678a5093dd88c5e26f",
+    "url": "/money_ok/static/css/main.18b3f317.chunk.css"
+  },
+  {
+    "revision": "9b27a9a95d18ec08ecc4",
+    "url": "/money_ok/static/js/2.003fd295.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/money_ok/static/js/2.003fd295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb678a5093dd88c5e26f",
+    "url": "/money_ok/static/js/main.a9e33f05.chunk.js"
+  },
+  {
+    "revision": "6f0d82f74fe3799c9e44",
+    "url": "/money_ok/static/js/runtime-main.4c6f05d1.js"
+  }
+]);
